37c43850bffbab49e9d8a3b199685d38f32376a8
davidineli857-ops <davidineli857@gmail.com>
2026-08-16 16:06:36 -0300

    update erro tentando colocar interações

--- effects.js
+++ effects.js
@@ -1,7 +1,30 @@
-setTimeout(function() {
+/*setTimeout(function() {
     document.body.classList.add("saida")
 
 
     setTimeout(function() {
          window.location.href = "abertura.html"
         },2700) },3450)
+
+
+
+
+function noturno() {
+   document.body.classList.toggle("escuro")
+ 
+ 
+  
+}
+
+ let padrao = document.querySelector("#tema")
+  
+ padrao.addEventListener ("click",function troca() {
+      if (padrao.value == "Modo Escuro") {
+         padrao.value = "Padrão"
+      }     else if (padrao.value = "Padrão") {
+       padrao.value = "Modo Escuro"
+      }
+ }
+) */
+
+
